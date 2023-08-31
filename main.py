66590a1e1f555e00643e73e4298de395ddafe1fe
Umair khan <89532763+umairrrkhan@users.noreply.github.com>
2023-08-31 16:51:33 +0530

    Update main.py
    the more accurate motion filtering

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import cv2 as cv
 
 video = cv.VideoCapture('Messi Incredible Goal vs Athletic Bilbao - English Commentary.mp4')
 
-output_video = cv.VideoWriter('output_video 2.mp4', cv.VideoWriter_fourcc(*'mp4v'), 30, (0, 0), isColor=False)
-
 # video = cv.videocapture(0)  you can use this code for camera dont need to download the video
 
 subtractor = cv.createBackgroundSubtractorMOG2(20 , 50)
@@ -21,6 +19,5 @@ while True:
 
 cv.destroyAllWindows()
 video.release()
-output_video.release()
 
 
